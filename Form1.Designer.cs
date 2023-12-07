e1e1ea16da4639aec219566ad97dffbbb9d7b17d
Lawrence C <lawrencecaudle870@gmail.com>
2023-12-07 11:32:06 -0500

    added radio btns, removed combo boxes, added labels

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,22 +31,32 @@
             comboBox1 = new ComboBox();
             richTextBox1 = new RichTextBox();
             richTextBox2 = new RichTextBox();
-            comboBox2 = new ComboBox();
-            comboBox3 = new ComboBox();
-            textBox1 = new TextBox();
+            groupBox1 = new GroupBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            groupBox2 = new GroupBox();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(94, 61);
+            comboBox1.Location = new Point(94, 128);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(525, 33);
+            comboBox1.Size = new Size(560, 33);
             comboBox1.TabIndex = 0;
             // 
             // richTextBox1
             // 
-            richTextBox1.Location = new Point(94, 124);
+            richTextBox1.Location = new Point(94, 198);
             richTextBox1.Name = "richTextBox1";
             richTextBox1.Size = new Size(225, 59);
             richTextBox1.TabIndex = 1;
@@ -54,48 +64,144 @@
             // 
             // richTextBox2
             // 
-            richTextBox2.Location = new Point(403, 124);
+            richTextBox2.Location = new Point(438, 198);
             richTextBox2.Name = "richTextBox2";
             richTextBox2.Size = new Size(216, 59);
             richTextBox2.TabIndex = 2;
             richTextBox2.Text = "";
             // 
-            // comboBox2
+            // groupBox1
             // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(94, 180);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(225, 33);
-            comboBox2.TabIndex = 3;
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(94, 255);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(225, 142);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
             // 
-            // comboBox3
+            // radioButton1
             // 
-            comboBox3.FormattingEnabled = true;
-            comboBox3.Location = new Point(403, 180);
-            comboBox3.Name = "comboBox3";
-            comboBox3.Size = new Size(216, 33);
-            comboBox3.TabIndex = 4;
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(30, 48);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(141, 29);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // radioButton2
             // 
-            textBox1.Location = new Point(94, 253);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(150, 31);
-            textBox1.TabIndex = 5;
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(30, 92);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(141, 29);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(radioButton4);
+            groupBox2.Controls.Add(radioButton3);
+            groupBox2.Location = new Point(438, 255);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(216, 142);
+            groupBox2.TabIndex = 7;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(23, 48);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(141, 29);
+            radioButton3.TabIndex = 0;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(23, 92);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(141, 29);
+            radioButton4.TabIndex = 1;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "radioButton4";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(94, 424);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 25);
+            label1.TabIndex = 8;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(348, 214);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 25);
+            label2.TabIndex = 9;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(206, 424);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 25);
+            label3.TabIndex = 10;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(348, 56);
+            label4.Name = "label4";
+            label4.Size = new Size(59, 25);
+            label4.TabIndex = 11;
+            label4.Text = "label4";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(542, 419);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 12;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
-            Controls.Add(comboBox3);
-            Controls.Add(comboBox2);
+            ClientSize = new Size(800, 499);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Controls.Add(richTextBox2);
             Controls.Add(richTextBox1);
             Controls.Add(comboBox1);
             Name = "Form1";
             Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -105,8 +211,16 @@
         private ComboBox comboBox1;
         private RichTextBox richTextBox1;
         private RichTextBox richTextBox2;
-        private ComboBox comboBox2;
-        private ComboBox comboBox3;
-        private TextBox textBox1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private GroupBox groupBox2;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
     }
 }
