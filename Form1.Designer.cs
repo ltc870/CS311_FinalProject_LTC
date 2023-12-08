3cbb546ab2e2b56e4a0bb6bca6834e8214aaa464
Lawrence C <lawrencecaudle870@gmail.com>
2023-12-07 19:41:13 -0500

    added properties to controls

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CS311_FinalProject_LTC
 {
-    partial class Form1
+    partial class formUnitConverter
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,53 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            comboBox1 = new ComboBox();
-            groupBox1 = new GroupBox();
-            radioButton1 = new RadioButton();
+            cboBoxUnitPicker = new ComboBox();
+            grpBoxConverGrp1 = new GroupBox();
             radioButton2 = new RadioButton();
-            groupBox2 = new GroupBox();
-            radioButton3 = new RadioButton();
+            radioButton1 = new RadioButton();
+            grpBoxConverGrp2 = new GroupBox();
             radioButton4 = new RadioButton();
+            radioButton3 = new RadioButton();
             label1 = new Label();
-            label2 = new Label();
+            lblEqualSign = new Label();
             label3 = new Label();
-            label4 = new Label();
+            lblTitle = new Label();
             button1 = new Button();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            groupBox1.SuspendLayout();
-            groupBox2.SuspendLayout();
+            textBoxUnit1 = new TextBox();
+            textBoxUnit2 = new TextBox();
+            grpBoxConverGrp1.SuspendLayout();
+            grpBoxConverGrp2.SuspendLayout();
             SuspendLayout();
             // 
-            // comboBox1
+            // cboBoxUnitPicker
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(94, 128);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(560, 33);
-            comboBox1.TabIndex = 0;
+            cboBoxUnitPicker.FormattingEnabled = true;
+            cboBoxUnitPicker.Items.AddRange(new object[] { "Length", "Mass", "Temperature" });
+            cboBoxUnitPicker.Location = new Point(94, 128);
+            cboBoxUnitPicker.Name = "cboBoxUnitPicker";
+            cboBoxUnitPicker.Size = new Size(560, 33);
+            cboBoxUnitPicker.TabIndex = 0;
             // 
-            // groupBox1
+            // grpBoxConverGrp1
             // 
-            groupBox1.Controls.Add(radioButton2);
-            groupBox1.Controls.Add(radioButton1);
-            groupBox1.Location = new Point(94, 255);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(225, 142);
-            groupBox1.TabIndex = 6;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "groupBox1";
-            // 
-            // radioButton1
-            // 
-            radioButton1.AutoSize = true;
-            radioButton1.Location = new Point(30, 48);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(141, 29);
-            radioButton1.TabIndex = 0;
-            radioButton1.TabStop = true;
-            radioButton1.Text = "radioButton1";
-            radioButton1.UseVisualStyleBackColor = true;
+            grpBoxConverGrp1.Controls.Add(radioButton2);
+            grpBoxConverGrp1.Controls.Add(radioButton1);
+            grpBoxConverGrp1.Location = new Point(94, 255);
+            grpBoxConverGrp1.Name = "grpBoxConverGrp1";
+            grpBoxConverGrp1.Size = new Size(225, 142);
+            grpBoxConverGrp1.TabIndex = 6;
+            grpBoxConverGrp1.TabStop = false;
+            grpBoxConverGrp1.Text = "groupBox1";
             // 
             // radioButton2
             // 
@@ -87,27 +77,27 @@
             radioButton2.Text = "radioButton2";
             radioButton2.UseVisualStyleBackColor = true;
             // 
-            // groupBox2
+            // radioButton1
             // 
-            groupBox2.Controls.Add(radioButton4);
-            groupBox2.Controls.Add(radioButton3);
-            groupBox2.Location = new Point(438, 255);
-            groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(255, 142);
-            groupBox2.TabIndex = 7;
-            groupBox2.TabStop = false;
-            groupBox2.Text = "groupBox2";
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(30, 48);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(141, 29);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // grpBoxConverGrp2
             // 
-            radioButton3.AutoSize = true;
-            radioButton3.Location = new Point(23, 48);
-            radioButton3.Name = "radioButton3";
-            radioButton3.Size = new Size(141, 29);
-            radioButton3.TabIndex = 0;
-            radioButton3.TabStop = true;
-            radioButton3.Text = "radioButton3";
-            radioButton3.UseVisualStyleBackColor = true;
+            grpBoxConverGrp2.Controls.Add(radioButton4);
+            grpBoxConverGrp2.Controls.Add(radioButton3);
+            grpBoxConverGrp2.Location = new Point(438, 255);
+            grpBoxConverGrp2.Name = "grpBoxConverGrp2";
+            grpBoxConverGrp2.Size = new Size(255, 142);
+            grpBoxConverGrp2.TabIndex = 7;
+            grpBoxConverGrp2.TabStop = false;
+            grpBoxConverGrp2.Text = "groupBox2";
             // 
             // radioButton4
             // 
@@ -120,6 +110,17 @@
             radioButton4.Text = "radioButton4";
             radioButton4.UseVisualStyleBackColor = true;
             // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(23, 48);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(141, 29);
+            radioButton3.TabIndex = 0;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
             // label1
             // 
             label1.AutoSize = true;
@@ -129,14 +130,15 @@
             label1.TabIndex = 8;
             label1.Text = "label1";
             // 
-            // label2
+            // lblEqualSign
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(348, 214);
-            label2.Name = "label2";
-            label2.Size = new Size(59, 25);
-            label2.TabIndex = 9;
-            label2.Text = "label2";
+            lblEqualSign.AutoSize = true;
+            lblEqualSign.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblEqualSign.Location = new Point(363, 218);
+            lblEqualSign.Name = "lblEqualSign";
+            lblEqualSign.Size = new Size(31, 32);
+            lblEqualSign.TabIndex = 9;
+            lblEqualSign.Text = "=";
             // 
             // label3
             // 
@@ -147,14 +149,15 @@
             label3.TabIndex = 10;
             label3.Text = "label3";
             // 
-            // label4
+            // lblTitle
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(348, 56);
-            label4.Name = "label4";
-            label4.Size = new Size(59, 25);
-            label4.TabIndex = 11;
-            label4.Text = "label4";
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblTitle.Location = new Point(297, 53);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(178, 32);
+            lblTitle.TabIndex = 11;
+            lblTitle.Text = "Unit Converter";
             // 
             // button1
             // 
@@ -165,60 +168,62 @@
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // textBoxUnit1
             // 
-            textBox1.Location = new Point(94, 218);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(225, 31);
-            textBox1.TabIndex = 13;
+            textBoxUnit1.Location = new Point(94, 218);
+            textBoxUnit1.Name = "textBoxUnit1";
+            textBoxUnit1.Size = new Size(225, 31);
+            textBoxUnit1.TabIndex = 13;
+            textBoxUnit1.TextAlign = HorizontalAlignment.Center;
             // 
-            // textBox2
+            // textBoxUnit2
             // 
-            textBox2.Location = new Point(438, 218);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(255, 31);
-            textBox2.TabIndex = 14;
+            textBoxUnit2.Location = new Point(438, 218);
+            textBoxUnit2.Name = "textBoxUnit2";
+            textBoxUnit2.Size = new Size(255, 31);
+            textBoxUnit2.TabIndex = 14;
+            textBoxUnit2.TextAlign = HorizontalAlignment.Center;
             // 
-            // Form1
+            // formUnitConverter
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 499);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(textBoxUnit2);
+            Controls.Add(textBoxUnit1);
             Controls.Add(button1);
-            Controls.Add(label4);
+            Controls.Add(lblTitle);
             Controls.Add(label3);
-            Controls.Add(label2);
+            Controls.Add(lblEqualSign);
             Controls.Add(label1);
-            Controls.Add(groupBox2);
-            Controls.Add(groupBox1);
-            Controls.Add(comboBox1);
-            Name = "Form1";
+            Controls.Add(grpBoxConverGrp2);
+            Controls.Add(grpBoxConverGrp1);
+            Controls.Add(cboBoxUnitPicker);
+            Name = "formUnitConverter";
             Text = "Form1";
-            groupBox1.ResumeLayout(false);
-            groupBox1.PerformLayout();
-            groupBox2.ResumeLayout(false);
-            groupBox2.PerformLayout();
+            grpBoxConverGrp1.ResumeLayout(false);
+            grpBoxConverGrp1.PerformLayout();
+            grpBoxConverGrp2.ResumeLayout(false);
+            grpBoxConverGrp2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private ComboBox comboBox1;
-        private GroupBox groupBox1;
+        private ComboBox cboBoxUnitPicker;
+        private GroupBox grpBoxConverGrp1;
         private RadioButton radioButton2;
         private RadioButton radioButton1;
-        private GroupBox groupBox2;
+        private GroupBox grpBoxConverGrp2;
         private RadioButton radioButton4;
         private RadioButton radioButton3;
         private Label label1;
-        private Label label2;
+        private Label lblEqualSign;
         private Label label3;
-        private Label label4;
+        private Label lblTitle;
         private Button button1;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private TextBox textBoxUnit1;
+        private TextBox textBoxUnit2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,8 @@
 namespace CS311_FinalProject_LTC
 {
-    public partial class Form1 : Form
+    public partial class formUnitConverter : Form
     {
-        public Form1()
+        public formUnitConverter()
         {
             InitializeComponent();
         }
